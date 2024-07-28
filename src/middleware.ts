7efb703da24248a04954236d4e3401b89442cce5
Rick Georges <94584894+rickgeorges01@users.noreply.github.com>
2024-07-28 02:43:26 +0200

    Application set-up (Nextjs,Shadcn,Ckerk) and Home page design

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import {authMiddleware, clerkMiddleware} from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes:["/"],
+});
+
+//This config that specifies the matcher whom  is a tool or component that decides whether a certain condition is met or not.
+//matcher is basically a bunch of regex to specify when this middleware should run
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
